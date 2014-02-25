c78363f37503a488fd0a5c3db72246f7c73d30f1
Naoki Otani <naoki.otani.zg@gmail.com>
2014-02-25 17:34:40 +0900

    商品管理ページと商品追加ページの実装

--- ma/goods_main.php
+++ ma/goods_main.php
@@ -1,5 +1,5 @@
-<?php 
-	require_once("./common.php");
+<?php
+//	require_once("./common.php");
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -7,13 +7,52 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 <meta http-equiv="Content-Style-Type" content="text/css">
-<link rel="stylesheet" href="my.css" content="text/css">
+<link rel="stylesheet" href="../my.css" content="text/css">
 </head>
 <body>
 <h1>TSUKASA　Shop</h1>
 <!-- ヘッダー -->
 
 <!-- コンテンツ -->
+
+	<p><a href="goods_add.php">新規追加</a></p>
+	<p><a href="../maintenance.php">戻る</a></p>
+
+
+	<?php
+	  $db = new PDO("mysql:dbname=tsukasadb","root","root");
+	  $db->query('SET NAMES utf8;');
+		$sql = $db->prepare('SELECT sid,sname,kakaku,cname FROM shouhin JOIN category ON shouhin.cid=category.cid;');
+		$sql->execute();
+
+		$all = $sql->fetchAll();
+		$sql = null;
+
+	  echo '<table>';
+	  echo '<tr><th>商品名</th><th>カテゴリ</th><th>価格</th><th>編集</th><th>削除</th></tr>';
+
+	  foreach( $all as $data ){
+	    echo '<tr>';
+	    // 商品名表示
+	    echo "<td>{$data['sname']}</td>";
+	    // カテゴリ表示
+	    if( $data['cname'] == NULL ){
+	    	echo "指定なし";
+	    }else{
+		    echo "<td>{$data['cname']}</td>";
+	    }
+	    // 価格表示
+	    echo "<td>{$data['kakaku']}</td>";
+	    // 編集リンク
+	    echo '<td><a href="ma/goods_edit.php?sid=' . $data['sid'] . '">';
+	    echo '<button>' . '編集' . '</button></td>';
+	    //　削除リンク
+	    echo '<td><a href="ma/goods_delete.php?sid=' . $data['sid'] . '">';
+	    echo '<button>' . '削除' . '</button></td>';
+
+	    echo '</tr>';
+	  }
+	?>
 
 </body>
 </html>
