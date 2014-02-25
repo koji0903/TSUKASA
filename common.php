502af035881a07d8ba804d08f085a7994f7c940b
Koji HIJIKURO <koji0903@gmail.com>
2014-02-25 13:11:54 +0900

    getDB関数を更新（引数を追加)

--- common.php
+++ common.php
@@ -7,8 +7,18 @@ function db()
 	return $db;
 }
 // GIDの取得関数
-function getGID()
+function getGID($uid)
 {
 	$db = db();
+	$sql = $db->prepare('SELECT gid FROM user WHERE uid = ?;');
+	$sql->bindValue(1,$uid);
+	if ( $sql->execute() ){
+		$data = $sql->fetch(PDO::FETCH_ASSOC);
+		return $data['gid'];
+	}
+	else
+	{
+		// データベースアクセス失敗
+	}
 }
 ?>
